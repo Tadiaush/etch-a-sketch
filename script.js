3e0f9e39850916b152a0693625d55087c2c554c6
Tadiaush <tadas.bobinas@gmail.com>
2021-04-28 22:13:51 +0300

    Adding updated version.

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
 const gridContainer = document.querySelector("#grid-container");
 const resetButton = document.querySelector('#reset');
 
-window.addEventListener('load',setDefaultGrid);
-resetButton.addEventListener('click',resetGrid);
+window.addEventListener('load', setDefaultGrid);
+resetButton.addEventListener('click', resetGrid);
 
-function setDefaultGrid(){
+function setDefaultGrid() {
     setGrideSize(16);
     createGrid(16);
 
 }
 
-function setGrideSize(size){
+function setGrideSize(size) {
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 }
 
 function createGrid(size) {
-        console.log('starting createGrid');
-        for (let index = 0; index < (size * 2); index++) {
-            const gridElement = document.createElement('div');
-            gridElement.classList.add('grid-element');
-
-            gridContainer.appendChild(gridElement);
-        }
-
-        let newGrid = gridContainer.querySelectorAll('div');
-        newGrid.forEach(oneDiv => oneDiv.addEventListener('mouseover', colorGrid));
+    console.log('starting createGrid');
+    for (let index = 0; index < (size * size); index++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridContainer.appendChild(gridElement);
     }
+
+    let newGrid = gridContainer.querySelectorAll('div');
+    newGrid.forEach(oneDiv => oneDiv.addEventListener('mouseover', colorGrid));
+}
 
 function colorGrid() {
-        
-    this.style.backgroundColor = 'black';
+    let randomColor = '#' + ((1 << 24) * Math.random() | 0).toString(16);
+    if (this.style.backgroundColor === '') {
+        this.style.backgroundColor = randomColor;
     }
+}
 
-function resetGrid(){
+function resetGrid() {
     const divArray = Array.from(gridContainer.childNodes);
     divArray.forEach(divElement => divElement.parentNode.removeChild(divElement));
+    newGrid();
+}
 
-    setDefaultGrid();
+function newGrid() {
+    let gridSize = prompt("Enter value of the Grid: (possible value between 0 and 100)");
+
+    if (gridSize > 0 && gridSize <= 100 && Number.isInteger(parseInt(gridSize))) {
+        setGrideSize(gridSize);
+        createGrid(gridSize);
+    } else {
+        alert('The number is not in between 1 and 100');
+        setDefaultGrid();
+    }
+
 }
 
 
